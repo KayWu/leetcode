8b2c8754b2675d88ef15d4d1d48e68ee70479161
Kay Wu <wuyingying.kay@gmail.com>
2018-12-09 15:26:17 +0800

    refactor: improve solution for Linked List Cycle

--- src/main/java/com/kay/leetcode/LinkedListCycleSolution.java
+++ src/main/java/com/kay/leetcode/LinkedListCycleSolution.java
@@ -6,23 +6,15 @@ package com.kay.leetcode;
 public class LinkedListCycleSolution {
 
     public boolean hasCycle(ListNode head) {
-        if (head == null) {
-            return false;
-        }
-        ListNode fast = head.next, slow = head;
+        ListNode fast = head, slow = head;
 
-        while (fast != null && slow != null) {
+        while (fast != null && fast.next != null && slow != null) {
+            slow = slow.next;
+            fast = fast.next.next;
             if (fast == slow) {
                 return true;
             }
-            slow = slow.next;
-            if (fast.next != null) {
-                fast = fast.next.next;
-            } else {
-                return false;
-            }
         }
         return false;
-
     }
 }
