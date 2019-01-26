a053ca03acca67778fb455ee045f85650acb5514
Kay Wu <wuyingying.kay@gmail.com>
2019-01-26 21:14:02 +0800

    feat: complete Level Order Traversal

---
+++ src/main/java/com/kay/leetcode/LevelOrderTraversalSolution.java
@@ -0,0 +1,37 @@
+package com.kay.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ */
+public class LevelOrderTraversalSolution {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> orderList = new ArrayList<>();
+        if (root == null) {
+            return orderList;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                currentLevel.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            orderList.add(currentLevel);
+        }
+        return orderList;
+    }
+}
---
+++ src/test/java/com/kay/leetcode/LevelOrderTraversalSolutionTest.java
@@ -0,0 +1,31 @@
+package com.kay.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LevelOrderTraversalSolutionTest {
+
+    private LevelOrderTraversalSolution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new LevelOrderTraversalSolution();
+    }
+
+    @Test
+    void sample() {
+        assertThat(solution.levelOrder(TreeUtil.buildTree(Arrays.asList(3, 9, 20, null, null, 15, 7))),
+                contains(Arrays.asList(3), Arrays.asList(9, 20), Arrays.asList(15, 7)));
+    }
+
+    @Test
+    void emptyTree() {
+        assertTrue(solution.levelOrder(null).isEmpty());
+    }
+}
