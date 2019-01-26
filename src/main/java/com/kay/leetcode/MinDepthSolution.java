fe4565f910228b30d7790e2c04fb7669a163cd19
Kay Wu <wuyingying.kay@gmail.com>
2019-01-26 23:52:29 +0800

    feat: complete Minimum Depth of Binary Tree

---
+++ src/main/java/com/kay/leetcode/MinDepthSolution.java
@@ -0,0 +1,36 @@
+package com.kay.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/
+ */
+public class MinDepthSolution {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        int depth = 0;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            depth++;
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+}
