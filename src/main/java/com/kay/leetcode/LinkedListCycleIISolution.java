e40985f5deac12d4e62ccf46ba35949bf229bac2
Kay Wu <kay.wu@alo7.com>
2019-01-30 15:17:19 +0800

    docs: add doc for Linked List Cycle II

--- src/main/java/com/kay/leetcode/LinkedListCycleIISolution.java
+++ src/main/java/com/kay/leetcode/LinkedListCycleIISolution.java
@@ -7,7 +7,8 @@ public class LinkedListCycleIISolution {
 
     /**
      * 假设 List 在 cycle 前的长度为 x, cycle 长度为 y, fast 与 slow 相遇在 cycle 过后 z 长度的位置
-     * 2(x + ay + z) = x + b + z => x + z = (b-2a)y
+     * 2(x + ay + z) = x + by + z => x + z = (b-2a)y
+     * x + by + z + x => x + 2(b - a)y 即 cycle 起点
      * 由于 slow 已经在 cycle 过后 z 长度，它再走 x 则会回到 cycle 起点
      * @param head
      * @return
