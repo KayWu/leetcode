efd24c179f96abb2c9184c1b85d60b2039e295c5
Kay Wu <kay.wu@alo7.com>
2019-02-01 13:48:03 +0800

    refactor: use local variable instead of instance variable in N-Queens II

--- src/main/java/com/kay/leetcode/NQueensIISolution.java
+++ src/main/java/com/kay/leetcode/NQueensIISolution.java
@@ -5,32 +5,30 @@ package com.kay.leetcode;
  */
 public class NQueensIISolution {
 
-    private int count;
-
     public int totalNQueens(int n) {
         if (n < 1) {
             throw new IllegalArgumentException();
         }
-        count = 0;
-        dfs(0, n, new boolean[n], new boolean[2 * n], new boolean[2 * n]);
-        return count;
+        return dfs(0, n, new boolean[n], new boolean[2 * n], new boolean[2 * n], 0);
     }
 
-    private void dfs(int currentRow, int n, boolean[] cols, boolean[] add, boolean[] minus) {
+    private int dfs(int currentRow, int n, boolean[] cols, boolean[] add, boolean[] minus, int count) {
         if (currentRow >= n) {
             count++;
-        }
-        for (int i = 0; i < n; i++) {
-            if (!cols[i] && !add[currentRow + i] && !minus[currentRow - i + n]) {
-                cols[i] = true;
-                add[currentRow + i] = true;
-                minus[currentRow - i + n] = true;
-                dfs(currentRow + 1, n, cols, add, minus);
-                cols[i] = false;
-                add[currentRow + i] = false;
-                minus[currentRow - i + n] = false;
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (!cols[i] && !add[currentRow + i] && !minus[currentRow - i + n]) {
+                    cols[i] = true;
+                    add[currentRow + i] = true;
+                    minus[currentRow - i + n] = true;
+                    count = dfs(currentRow + 1, n, cols, add, minus, count);
+                    cols[i] = false;
+                    add[currentRow + i] = false;
+                    minus[currentRow - i + n] = false;
+                }
             }
         }
+        return count;
     }
 
 }
