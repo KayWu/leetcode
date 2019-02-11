01b79be46fa18eab7bf30cc7899bccfb9a88a1bb
Kay Wu <wuyingying.kay@gmail.com>
2019-02-11 19:58:52 +0800

    feat: complete Implement Trie

---
+++ src/main/java/com/kay/leetcode/Trie.java
@@ -0,0 +1,69 @@
+package com.kay.leetcode;
+
+/**
+ * https://leetcode.com/problems/implement-trie-prefix-tree/
+ */
+public class Trie {
+
+    private static final int R = 26;
+    private static char BEGIN_LETTER = 'a';
+
+    private static class Node {
+        private Node[] next = new Node[R];
+        private boolean isFinalLetter;
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Trie() {
+        root = new Node();
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        Node node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (node.next[c - BEGIN_LETTER] == null) {
+                node.next[c - BEGIN_LETTER] = new Node();
+            }
+            node = node.next[c - BEGIN_LETTER];
+            if (i == word.length() - 1) {
+                node.isFinalLetter = true;
+            }
+        }
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        Node node = findNode(word);
+        return node != null && node.isFinalLetter;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        return findNode(prefix) != null;
+    }
+
+    private Node findNode(String prefix) {
+        Node node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            node = node.next[c - BEGIN_LETTER];
+            if (node == null) {
+                return null;
+            }
+        }
+        return node;
+    }
+}
+
