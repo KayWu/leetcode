deb460aea314e0fc4ae1d93f10f582584623ac56
Kay Wu <wuyingying.kay@gmail.com>
2019-02-12 16:29:04 +0800

    feat: improve solution for Counting Bits

--- src/main/java/com/kay/leetcode/CountingBitsSolution.java
+++ src/main/java/com/kay/leetcode/CountingBitsSolution.java
@@ -10,14 +10,8 @@ public class CountingBitsSolution {
         }
         int[] result = new int[num + 1];
         result[0] = 0;
-        int lastPowerOfTwo = 1;
         for (int i = 1; i <= num; i++) {
-            if ((i & (i - 1)) == 0) {
-                lastPowerOfTwo = i;
-                result[i] = 1;
-            } else {
-                result[i] = 1 + result[i - lastPowerOfTwo];
-            }
+            result[i] = 1 + result[i & (i - 1)];
         }
         return result;
     }
