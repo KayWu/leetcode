400808c7f6ce1446c01a67862ce83734f3b4baf6
Kay Wu <kay.wu@alo7.com>
2019-02-20 12:11:11 +0800

    feat: improve solution for Coin Change

--- src/main/java/com/kay/leetcode/CoinChangeSolution.java
+++ src/main/java/com/kay/leetcode/CoinChangeSolution.java
@@ -8,35 +8,19 @@ import java.util.Arrays;
 public class CoinChangeSolution {
 
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
-        if (amount < 0 || coins.length == 0) {
-            return -1;
-        }
-
+        int max = amount + 1;
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE / 2);
-
-        for (int i = 0; i < coins.length; i++) {
-            if (coins[i] <= amount) {
-                dp[coins[i]] = 1;
-            }
-        }
-
+        Arrays.fill(dp, max);
+        dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
                 int coin = coins[j];
-                if (i > coin) {
+                if (i >= coin) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        if (dp[amount] <= amount) {
-            return dp[amount];
-        } else {
-            return -1;
-        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 
 }
