06b16bedbb6e54813641d00a4189f065453302f0
Kay Wu <wuyingying.kay@gmail.com>
2019-02-25 23:33:41 +0800

    feat: complete Add Two Number

---
+++ src/test/java/com/kay/leetcode/AddTwoNumberSolutionTest.java
@@ -0,0 +1,36 @@
+package com.kay.leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class AddTwoNumberSolutionTest {
+    private AddTwoNumberSolution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new AddTwoNumberSolution();
+    }
+
+    @Test
+    void sample() {
+        ListNode head = solution.addTwoNumbers(LinkedListUtil.buildLinkedList(2, 4, 3),
+                LinkedListUtil.buildLinkedList(5, 6, 4));
+        LinkedListUtil.checkOrder(head, Arrays.asList(7, 0, 8));
+    }
+
+    @Test
+    void withCarry() {
+        ListNode head = solution.addTwoNumbers(LinkedListUtil.buildLinkedList(5, 5, 4),
+                LinkedListUtil.buildLinkedList(8, 8, 6));
+        LinkedListUtil.checkOrder(head, Arrays.asList(3, 4, 1, 1));
+    }
+
+    @Test
+    void withNoCarry() {
+        ListNode head = solution.addTwoNumbers(LinkedListUtil.buildLinkedList(5, 5, 4),
+                LinkedListUtil.buildLinkedList(4, 4, 5));
+        LinkedListUtil.checkOrder(head, Arrays.asList(9, 9, 9));
+    }
+}
