7ad4dd276590a896cbdb293af83da6dd2d056c49
Kay Wu <wuyingying.kay@gmail.com>
2019-03-18 23:15:01 +0800

    refactor: refactor Majority Element

--- src/main/java/com/kay/leetcode/MajorityElement.java
+++ src/main/java/com/kay/leetcode/MajorityElement.java
@@ -5,14 +5,15 @@ package com.kay.leetcode;
  */
 public class MajorityElement {
     public int majorityElement(int[] nums) {
-        int candidate = nums[0];
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (count <= 0) {
-                candidate = nums[i];
+        int majority = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            count += (nums[i] == majority ? 1 : -1);
+            if (count < 0) {
+                majority = nums[i];
+                count = 1;
             }
-            count += (candidate == nums[i] ? 1 : -1);
         }
-        return candidate;
+        return majority;
     }
 }
