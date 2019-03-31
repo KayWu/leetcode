b35b62e68ba9fee06e61f9a549f393956fe161f5
Kay Wu <wuyingying.kay@gmail.com>
2019-03-31 15:55:51 +0800

    feat: complete Next Greater Node In Linked List

---
+++ src/main/java/com/kay/leetcode/NextLargerNodes.java
@@ -0,0 +1,28 @@
+package com.kay.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/next-greater-node-in-linked-list/
+ */
+public class NextLargerNodes {
+
+    public int[] nextLargerNodes(ListNode head) {
+        List<Integer> A = new ArrayList<>();
+        for (ListNode node = head; node != null; node = node.next) {
+            A.add(node.val);
+        }
+        int[] ans = new int[A.size()];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < A.size(); i++) {
+            while (!stack.isEmpty() && A.get(stack.peek()) < A.get(i)) {
+                ans[stack.pop()] = A.get(i);
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+}
---
+++ src/test/java/com/kay/leetcode/NextLargerNodesTest.java
@@ -0,0 +1,26 @@
+package com.kay.leetcode;
+
+import com.kay.leetcode.utils.LinkedListUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class NextLargerNodesTest {
+
+    private NextLargerNodes solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new NextLargerNodes();
+    }
+
+    @Test
+    void sample() {
+        assertThat(solution.nextLargerNodes(LinkedListUtil.buildLinkedList(2, 1, 5)), equalTo(new int[]{5, 5, 0}));
+        assertThat(solution.nextLargerNodes(LinkedListUtil.buildLinkedList(2, 7, 4, 3, 5)), equalTo(new int[]{7, 0, 5, 5, 0}));
+        assertThat(solution.nextLargerNodes(LinkedListUtil.buildLinkedList(1, 7, 5, 1, 9, 2, 5, 1)), equalTo(new int[]{7, 9, 9, 9, 0, 5, 0, 0}));
+    }
+
+}
