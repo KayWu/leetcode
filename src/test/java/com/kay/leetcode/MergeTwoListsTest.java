8b6463187f32077758e0adc21633aef8ff6cdc4c
Kay Wu <kay.wu@alo7.com>
2019-12-26 10:43:54 +0800

    feat: add Merge Two Lists

---
+++ src/test/java/com/kay/leetcode/MergeTwoListsTest.java
@@ -0,0 +1,43 @@
+package com.kay.leetcode;
+
+import com.kay.utils.LinkedListUtil;
+import com.kay.utils.ListNode;
+import com.sun.tools.javac.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class MergeTwoListsTest {
+
+    private MergeTwoLists solution;
+
+
+    @BeforeEach
+    void setUp() {
+        solution = new MergeTwoLists();
+    }
+
+    @Test
+    void twoEmptyList() {
+        ListNode head = solution.mergeTwoLists(null, null);
+        assertNull(head);
+    }
+
+
+    @Test
+    void oneEmptyList() {
+        ListNode head = solution.mergeTwoLists(null, LinkedListUtil.buildLinkedList(1, 3, 4));
+        assertEquals(List.of(1, 3, 4), LinkedListUtil.orderPresent(head));
+    }
+
+
+    @Test
+    void sample() {
+        ListNode head = solution.mergeTwoLists(LinkedListUtil.buildLinkedList(1, 2, 4), LinkedListUtil.buildLinkedList(1, 3, 4));
+        assertEquals(List.of(1, 1, 2, 3, 4, 4), LinkedListUtil.orderPresent(head));
+    }
+
+
+}
