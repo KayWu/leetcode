7846781836a372432cea8389b5b41e7346792d97
Kay Wu <wuyingying.kay@gmail.com>
2020-01-09 15:31:33 +0800

    feat: improve Reverse K Group

--- src/main/java/com/kay/leetcode/ReverseKGroup.java
+++ src/main/java/com/kay/leetcode/ReverseKGroup.java
@@ -8,57 +8,36 @@ import com.kay.utils.ListNode;
 public class ReverseKGroup {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (k == 1) return head;
+        if (k <= 0) {
+            throw new IllegalArgumentException();
+        }
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode prev = dummy;
-        while (hasSize(prev, k)) {
-            prev = reverseList(prev, k);
-        }
-        return dummy.next;
-    }
-
-
-    /**
-     * Check the linked list has required size, not count the prev node
-     *
-     * @param prevNode
-     * @param requiredSize
-     * @return
-     */
-    private boolean hasSize(ListNode prevNode, int requiredSize) {
-        ListNode cur = prevNode;
-        int size = 0;
-        while (cur.next != null) {
-            size++;
-            cur = cur.next;
-            if (size >= requiredSize) {
-                return true;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        int i = 0;
+        while (fast.next != null) {
+            fast = fast.next;
+            i++;
+            // k nodes
+            if (i % k == 0) {
+                ListNode prev = fast.next;
+                ListNode cur = slow.next;
+                for (int j = 0; j < k; j++) {
+                    // reverse the list
+                    ListNode temp = cur.next;
+                    cur.next = prev;
+                    prev = cur;
+                    cur = temp;
+                }
+                ListNode temp = slow.next;
+                slow.next = fast;
+                slow = temp;
+                fast = temp;
             }
         }
-        return false;
+        return dummy.next;
+
     }
 
-    /**
-     * reverse linked list of certain size and return the last node of the list
-     *
-     * @param prevNode
-     * @param size
-     * @return
-     */
-    private ListNode reverseList(ListNode prevNode, int size) {
-        ListNode prev = prevNode, cur = prevNode.next;
-        int remainNode = size;
-        while (remainNode > 0) {
-            ListNode temp = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = temp;
-            remainNode--;
-        }
-        ListNode nextPrevNode = prevNode.next;
-        prevNode.next = prev;
-        nextPrevNode.next = cur;
-        return nextPrevNode;
-    }
 }
