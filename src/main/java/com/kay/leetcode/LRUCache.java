baa4b34f51da5c4364b1f01f11ee08efa8532060
Kay Wu <wuyingying.kay@gmail.com>
2020-01-31 16:07:47 +0800

    feat: improve LRU cache

--- src/main/java/com/kay/leetcode/LRUCache.java
+++ src/main/java/com/kay/leetcode/LRUCache.java
@@ -6,81 +6,78 @@ import java.util.Map;
 /**
  * https://leetcode.com/problems/lru-cache/
  */
-public class LRUCache {
+class LRUCache {
 
     private int capacity;
     private int count;
-    private Map<Integer, DLinkedNode> map;
-    private DLinkedNode headDummy;
-    private DLinkedNode tailDummy;
+    private Map<Integer, ListNode> map;
+    private ListNode head;
+    private ListNode tail;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
         this.count = 0;
-        this.map = new HashMap<>(capacity);
-        this.headDummy = new DLinkedNode();
-        this.tailDummy = new DLinkedNode();
-        headDummy.post = tailDummy;
-        tailDummy.pre = headDummy;
+        this.map = new HashMap<>();
+        this.head = new ListNode(-1, -1);
+        this.tail = new ListNode(-1, -1);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
     }
 
     public int get(int key) {
-        DLinkedNode node = map.get(key);
+        ListNode node = map.get(key);
         if (node == null) {
             return -1;
         }
+        deleteNode(node);
         moveToHead(node);
-        return node.value;
+        return node.val;
     }
 
+
     public void put(int key, int value) {
-        DLinkedNode node = map.get(key);
+        ListNode node = map.get(key);
         if (node == null) {
-            node = new DLinkedNode();
-            node.key = key;
-            node.value = value;
-            addNode(node);
-            map.put(key, node);
-            count++;
-            if (count > capacity) {
-                map.remove(removeTail().key);
+            node = new ListNode(key, value);
+            this.map.put(key, node);
+            moveToHead(node);
+            this.count++;
+            if (this.count > this.capacity) {
+                ListNode toDelete = this.tail.prev;
+                deleteNode(toDelete);
+                this.map.remove(toDelete.key);
                 count--;
             }
         } else {
-            node.value = value;
+            node.val = value;
+            deleteNode(node);
             moveToHead(node);
         }
 
     }
 
-    private void moveToHead(DLinkedNode node) {
-        removeNode(node);
-        addNode(node);
+    private void moveToHead(ListNode cur) {
+        cur.next = this.head.next;
+        cur.prev = this.head;
+        this.head.next.prev = cur;
+        this.head.next = cur;
     }
 
-    private void removeNode(DLinkedNode node) {
-        node.pre.post = node.post;
-        node.post.pre = node.pre;
+    private void deleteNode(ListNode cur) {
+        cur.prev.next = cur.next;
+        cur.next.prev = cur.prev;
     }
 
-    private DLinkedNode removeTail() {
-        DLinkedNode tail = tailDummy.pre;
-        removeNode(tailDummy.pre);
-        return tail;
-    }
-
-    private void addNode(DLinkedNode node) {
-        DLinkedNode postNode = this.headDummy.post;
-        postNode.pre = node;
-        node.post = postNode;
-        this.headDummy.post = node;
-        node.pre = this.headDummy;
-    }
-
-    private static class DLinkedNode {
+    private static class ListNode {
+        ListNode prev;
+        ListNode next;
         int key;
-        int value;
-        DLinkedNode pre;
-        DLinkedNode post;
+        int val;
+
+        ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+
     }
 }
