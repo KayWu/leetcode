da4bd92d1fcd73c8a957bfa164f4f9eb4638f494
Kay Wu <wuyingying.kay@gmail.com>
2020-02-01 14:22:28 +0800

    ci: move maven to gradle

--- src/test/java/com/kay/leetcode/RemoveNthFromEndTest.java
+++ src/test/java/com/kay/leetcode/RemoveNthFromEndTest.java
@@ -2,8 +2,9 @@ package com.kay.leetcode;
 
 import com.kay.utils.LinkedListUtil;
 import com.kay.utils.ListNode;
-import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
